d4cdbd1390fba5ff2b9edc2a933bac2c29c4f36f
RaffaeleBarbato <raffaele.barbato1997@gmail.com>
2022-02-08 09:55:29 +0100

    added pat

--- git_example/src/git_example/GitTest.java
+++ git_example/src/git_example/GitTest.java
@@ -4,5 +4,6 @@ public class GitTest {
 	public static void main(String[] args) 
 	{
 		System.out.println("second content");
+		System.out.println("the PAT is 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");
 	}
 }
